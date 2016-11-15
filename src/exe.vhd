ee37c69f3ab242af65db288994c0df2cf5eebb4d
ahaoyunqiu <shaoyunqiu@163.com>
2016-11-16 00:22:43 +0800

    finish exe.vhd, and simulate ok, but feel loss several port

--- src/exe.vhd
+++ src/exe.vhd
@@ -27,178 +27,117 @@ begin
 	aluop_o <= aluop_i ;
 	we_o <= we_i ;
 	waddr_o <= waddr_i ;
-	
-	process(alusel_i, aluop_i, reg0_i, reg1_i, stallreq)
-	--variable ans := DataBus := ZeroData ;
-	begin 
-		-- nop or int alu do nothing now
-		if(stallreq = StallNo and alusel_i = EXE_SEL_SPECIAL) then
-			null ;
-		else 
-			null ;
-		end if ;
-	end process ;
-	
-	process(alusel_i, aluop_i, reg0_i, reg1_i, stallreq) -- reg0_i(10:8), -- reg1_i(7:5)
-	variable ans : DataBus := ZeroData ;
-	begin
-		if(stallreq = StallNo and alusel_i = EXE_SEL_REG) then
-			case aluop_i is 
-				when EXE_OP_LI =>
-					ans := reg0_i ;
-				when EXE_OP_MFIH =>
-					ans := reg0_i ;
-				when EXE_OP_MFPC =>
-					ans := reg0_i ;
-				when EXE_OP_MOVE =>
-					ans := reg1_i ;
-				when EXE_OP_MTIH =>
-					ans := reg0_i ;
-				when EXE_OP_MTSP =>
-					ans := reg1_i ;
-				when others =>null ;
-			end case ;
-			wdata_o <= ans ;
-		else
-			null ;
-		end if ;
-	end process ;
-	
-	process(alusel_i, aluop_i, reg0_i, reg1_i, stallreq) -- when jump finish in ID and exe do nothing
-	variable ans : DataBus := ZeroData ;
-	begin
-		if(stallreq = StallNo and alusel_i = EXE_SEL_JUMP) then
-			null ;
-		else 
-			null ;
-		end if ;
-	end process ;
+	--wdata_o <= ZeroData ;
 	
 	process(alusel_i, aluop_i, reg0_i, reg1_i, stallreq)
 	variable ans : DataBus := ZeroData ;
 	begin
-		if(stallreq = StallNo and alusel_i = EXE_SEL_ARITH) then
-			case aluop_i is
-				when EXE_OP_SUBU =>
-					ans := reg0_i - reg1_i ;
-				when EXE_OP_ADDIU =>
-					ans := reg0_i + reg1_i ;
-				when EXE_OP_ADDIU3 =>
-					ans := reg0_i + reg1_i ;
-				when EXE_OP_ADDSP =>
-					ans := reg0_i + reg1_i ;
-				when EXE_OP_ADDU =>
-					ans := reg0_i + reg1_i ;
-				when others =>
-					null ;
+		if(stallreq = StallNo) then
+			case alusel_i is 
+				when EXE_SEL_ARITH =>
+					case aluop_i is
+						when EXE_OP_SUBU =>
+							ans := reg0_i - reg1_i ;
+						when EXE_OP_ADDIU =>
+							ans := reg0_i + reg1_i ;
+						when EXE_OP_ADDIU3 =>
+							ans := reg0_i + reg1_i ;
+						when EXE_OP_ADDSP =>
+							ans := reg0_i + reg1_i ;
+						when EXE_OP_ADDU =>
+							ans := reg0_i + reg1_i ;
+						when others =>
+							null ;
+					end case ;
+				when EXE_SEL_LOGIC =>
+					case aluop_i is 
+						when EXE_OP_AND =>
+							ans := reg0_i and reg1_i ;
+						when EXE_OP_NEG =>
+							ans := ZeroData - reg1_i ;
+						when EXE_OP_OR =>
+							ans := reg0_i or reg1_i ;
+						when others =>
+							null ;
+					end case ;
+				when EXE_SEL_CMP =>
+					case aluop_i is
+						when EXE_OP_CMP =>
+							if(reg0_i = reg1_i) then
+								ans := ZeroData ;
+							else
+								ans := "0000000000000001" ;
+							end if ;
+						when EXE_OP_CMPI =>
+							if(reg0_i = reg1_i) then
+								ans := ZeroData ;
+							else
+								ans := "0000000000000001" ;
+							end if ;
+						when others =>
+							null ;
+					end case ;
+				when EXE_SEL_SHIFT =>
+					case aluop_i is 
+						when EXE_OP_SLL =>
+							if(reg1_i = ZeroData) then
+								ans := to_stdlogicvector(TO_BITVECTOR(reg0_i) sll 8) ;
+							else
+								ans := to_stdlogicvector(TO_BITVECTOR(reg0_i) sll CONV_INTEGER(reg1_i)) ;
+							end if ;
+						when EXE_OP_SLLV =>
+							ans := to_stdlogicvector(TO_BITVECTOR(reg1_i) sll CONV_INTEGER(reg0_i)) ;
+						when EXE_OP_SRA =>
+							if(reg1_i = ZeroData) then
+								ans := to_stdlogicvector(TO_BITVECTOR(reg0_i) sra 8) ;
+							else
+								ans := to_stdlogicvector(TO_BITVECTOR(reg0_i) sra CONV_INTEGER(reg1_i)) ;
+							end if ;
+						when EXE_OP_SRL =>
+							if(reg1_i = ZeroData) then
+								ans := to_stdlogicvector(TO_BITVECTOR(reg0_i) srl 8) ;
+							else
+								ans := to_stdlogicvector(TO_BITVECTOR(reg0_i) srl CONV_INTEGER(reg1_i)) ;
+							end if ;
+						when others => 
+							null ;
+					end case ;
+				when EXE_SEL_REG =>
+					case aluop_i is 
+						when EXE_OP_LI =>
+							ans := reg0_i ;
+						when EXE_OP_MFIH =>
+							ans := reg0_i ;
+						when EXE_OP_MFPC =>
+							ans := reg0_i ;
+						when EXE_OP_MOVE =>
+							ans := reg1_i ;
+						when EXE_OP_MTIH =>
+							ans := reg0_i ;
+						when EXE_OP_MTSP =>
+							ans := reg1_i ;
+						when others =>null ;
+					end case ;
+				when EXE_SEL_LW =>
+					case aluop_i is
+						when EXE_OP_LW =>
+							ans := reg0_i + reg1_i ;
+						when EXE_OP_LW_SP =>
+							ans := reg0_i + reg1_i ;
+						when EXE_OP_SW =>
+							ans := reg0_i + reg1_i ;
+						when EXE_OP_SW_SP => 
+							ans := reg0_i + reg1_i ;
+						when others =>
+							null ;
+					end case ;
+				when others => null ; -- jump, and special ,alu do nothing
 			end case ;
-			wdata_o <= ans ;
 		else
 			null ;
 		end if ;
+		wdata_o <= ans ;
 	end process ;
 	
-	process(alusel_i, aluop_i, reg0_i, reg1_i, stallreq)
-	variable ans : DataBus := ZeroData ;
-	--variable shift_num : DataBus := ZeroData ;
-	begin
-		if(stallreq = StallNo and alusel_i = EXE_SEL_SHIFT) then
-			case aluop_i is 
-				when EXE_OP_SLL =>
-					if(reg1_i = ZeroData) then
-						ans := to_stdlogicvector(TO_BITVECTOR(reg0_i) sll 8) ;
-					else
-						ans := to_stdlogicvector(TO_BITVECTOR(reg0_i) sll CONV_INTEGER(reg1_i)) ;
-					end if ;
-				when EXE_OP_SLLV =>
-					ans := to_stdlogicvector(TO_BITVECTOR(reg1_i) sll CONV_INTEGER(reg0_i)) ;
-				when EXE_OP_SRA =>
-					if(reg1_i = ZeroData) then
-						ans := to_stdlogicvector(TO_BITVECTOR(reg0_i) sra 8) ;
-					else
-						ans := to_stdlogicvector(TO_BITVECTOR(reg0_i) sra CONV_INTEGER(reg1_i)) ;
-					end if ;
-				when EXE_OP_SRL =>
-					if(reg1_i = ZeroData) then
-						ans := to_stdlogicvector(TO_BITVECTOR(reg0_i) srl 8) ;
-					else
-						ans := to_stdlogicvector(TO_BITVECTOR(reg0_i) srl CONV_INTEGER(reg1_i)) ;
-					end if ;
-				when others => 
-					null ;
-			end case ;
-			wdata_o <= ans ;
-		else
-			null ;
-		end if ;
-	end process ;
-	
-	process(alusel_i, aluop_i, reg0_i, reg1_i, stallreq)
-	variable ans : DataBus := ZeroData ;
-	begin
-		if(stallreq = StallNo and alusel_i = EXE_SEL_LOGIC) then
-			case aluop_i is 
-				when EXE_OP_AND =>
-					ans := reg0_i and reg1_i ;
-				when EXE_OP_NEG =>
-					ans := ZeroData - reg1_i ;
-				when EXE_OP_OR =>
-					ans := reg0_i or reg1_i ;
-				when others =>
-					null ;
-			end case ;
-			wdata_o <= ans ;
-		else
-			null ;
-		end if ;
-	end process ;
-	
-	process(alusel_i, aluop_i, reg0_i, reg1_i, stallreq)
-	variable ans : DataBus := ZeroData ;
-	begin
-		if(stallreq = StallNo and alusel_i = EXE_SEL_CMP) then
-			case aluop_i is
-				when EXE_OP_CMP =>
-					if(reg0_i = reg1_i) then
-						ans := ZeroData ;
-					else
-						ans := "0000000000000001" ;
-					end if ;
-				when EXE_OP_CMPI =>
-					if(reg0_i = reg1_i) then
-						ans := ZeroData ;
-					else
-						ans := "0000000000000001" ;
-					end if ;
-				when others =>
-					null ;
-			end case ;
-			wdata_o <= ans ;
-		else
-			null ;
-		end if ;
-	end process ;
-	
-	process(alusel_i, aluop_i, reg0_i, reg1_i, stallreq)
-	variable ans : DataBus := ZeroData ;
-	begin
-		if(stallreq = StallNo and alusel_i = EXE_SEL_LW) then
-			case aluop_i is
-				when EXE_OP_LW =>
-					ans := reg0_i + reg1_i ;
-				when EXE_OP_LW_SP =>
-					ans := reg0_i + reg1_i ;
-				when EXE_OP_SW =>
-					ans := reg0_i + reg1_i ;
-				when EXE_OP_SW_SP => 
-					ans := reg0_i + reg1_i ;
-				when others =>
-					null ;
-			end case ;
-			wdata_o <= ans ;
-		else
-			null ;
-		end if ;
-	end process ;
 	
 end Behavioral ;
---
+++ test/exe_test.v
@@ -0,0 +1,92 @@
+`timescale 1ns / 1ns
+module exe_test ;
+
+// input
+reg [2:0] aluop_i  ;
+reg [2:0] alusel_i ;
+reg [15:0] reg0_i ;
+reg [15:0] reg1_i ;
+reg [3:0] waddr_i ;
+reg we_i ;
+reg stallreq ;
+
+//output
+wire [2:0] aluop_o ;
+wire we_o ;
+wire [3:0] waddr_o ;
+wire [15:0] wdata_o ;
+
+// Instantiate the Unit Under Test (UUT)
+exe uut(
+	.aluop_i(aluop_i),
+	.alusel_i(alusel_i),
+	.reg0_i(reg0_i),
+	.reg1_i(reg1_i),
+	.waddr_i(waddr_i),
+	.we_i(we_i),
+	.stallreq(stallreq),
+	.aluop_o(aluop_o),
+	.we_o(we_o),
+	.waddr_o(waddr_o),
+	.wdata_o(wdata_o)
+) ;
+
+initial begin
+	reg0_i = 0 ;
+	reg1_i = 0 ;
+	aluop_i = 0 ;
+	alusel_i = 0 ;
+	waddr_i = 1 ;
+	we_i = 1 ;
+	stallreq = 0 ;
+	
+	#10 ;
+	stallreq = 1 ;
+	alusel_i = 3'b111 ;
+	aluop_i = 3'b000 ;
+	reg0_i = 8 ;
+	reg1_i = 1 ;
+/* 	aluop_i = #10 3'b001 ;
+	aluop_i = #10 3'b010 ;
+	aluop_i = #10 3'b001 ;
+	aluop_i = #10 3'b100 ;
+	
+	#10
+	alusel_i = 3'b101 ;
+	aluop_i = 3'b000 ;
+	aluop_i = #10 3'b001 ;
+	aluop_i = #10 3'b010 ;
+	
+	# 20 ;
+	alusel_i = 3'b100 ;
+	aluop_i = 3'b000 ;
+	# 20 
+	reg0_i = 1 ;
+	# 20 ;
+	aluop_i = 3'b001 ;
+	# 20 ;
+	reg0_i = 8 ; */
+	#10 ;
+	alusel_i = 3'b110 ;
+	reg1_i = #5 0 ;
+	#10 ;
+	aluop_i = 3'b001 ;
+	reg1_i = 1 ;
+	#10 ;
+	aluop_i = 3'b010 ;
+	reg0_i = 16'b1000000001111111 ;
+	reg1_i = #5 0 ;
+	#10 ;
+	aluop_i = 3'b011 ;
+	reg1_i = #5 4 ;
+	#10 ;
+	alusel_i = 3'b000 ;
+	#10 ;
+	alusel_i = 3'b011 ;
+	aluop_i = 3'b000 ;
+	#10 ;
+	alusel_i = 3'b001 ;
+	#10 ;
+end
+
+endmodule
