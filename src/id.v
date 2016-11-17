31c0461e96038da35e45bf117c6e2fb8026f65bb
Auroraka <yangtianlong111@gmail.com>
2016-11-17 20:29:53 +0800

    id with some code

--- src/id.v
+++ src/id.v
@@ -9,21 +9,33 @@ module id(
 	input wire[`DataBus] reg0_data_i,
 	input wire[`DataBus] reg1_data_i,
 	
-	//inst_type
+	input wire[`RegAddrBus] ex_waddr_i,
+	input wire ex_we_i,
+	input wire[`DataBus] ex_wdata_i,
+
+	input wire[`RegAddrBus] mem_waddr_i,
+	input wire mem_we_i,
+	input wire[`DataBus] mem_wdata_i,
+
+	input[`DataBus] reg0_data_i,
+	input[`DataBus] reg1_data_i,
+
+
 	output reg[`AluSelBus] alusel_o,
 	output reg[`AluOpBus] aluop_o,
-	//read reg0 enable
 	output reg reg0_re_o,
-	output reg[`RegAddrBus] reg0_addr_o,
-	//read reg1 enable
 	output reg reg1_re_o,
-	output reg[`RegAddrBus] reg1_addr_o
-	//alu input 0,1
-	output reg[`DataBus] reg0_o,
-	output reg[`DataBus] reg1_o,
-	//write_enable
 	output reg we_o,
 	output reg[`RegAddrBus] waddr_o,	
+
+	output reg[`RegAddrBus] reg0_addr_o,
+	output reg[`RegAddrBus] reg1_addr_o
+	output reg[`DataBus] reg0_o,
+	output reg[`DataBus] reg1_o,
+
+	output reg stall_req,
+	output reg branch_flag_o,
+	output reg[`InstAddrBus] branch_addr_o
 );
 
 //more details see inst_list_types.pptx
@@ -40,23 +52,45 @@ wire[15:0] imml = inst_i[7]? {8{1'b1}, inst_i[7:0]}:{8'b0 , inst_i[7:0]};//inst[
 wire[15:0] immsu={11'b0 , op3};                                          //inst[4:0] -> unsigned extended imm
 wire[15:0] imms=op3[4]? {11{1'b1}, op3}:{11'b0 , op3};                   //inst[4:0] ->   signed extended imm
 
+	output reg[`AluSelBus] alusel_o,
+	output reg[`AluOpBus] aluop_o,
+	output reg reg0_re_o,
+	output reg reg1_re_o,
+	output reg we_o,
+	output reg[`RegAddrBus] waddr_o,	
+
+	output reg[`RegAddrBus] reg0_addr_o,
+	output reg[`RegAddrBus] reg1_addr_o
+	output reg[`DataBus] reg0_o,
+	output reg[`DataBus] reg1_o,
+
+	output reg stall_req,
+	output reg branch_flag_o,
+	output reg[`InstAddrBus] branch_addr_o
+
+
+
 always @ (*) begin
 	if (rst == `RstEnable) begin
 		//默认为nop,不从reg读数据,不写数据
 		alusel_o<=`EXE_SEL_SPECIAL;
 		aluop_o<=`EXE_OP_NOP;
 		reg0_re_o<=`ReadDisable;
-		reg0_addr_o<=`ZeroRegAddr;
 		reg1_re_o<=`ReadDisable;
+		we_o<=`WriteDisable;
+		waddr_o<=`ZeroDataAddr;
+		
+		reg0_addr_o<=`ZeroRegAddr;
 		reg1_addr_o<=`ZeroRegAddr;
 		reg0_o<=`ZeroData;
 		reg1_o<=`ZeroData;
-		we_o<=`WriteDisable;
-		waddr_o<=`ZeroDataAddr;
+
+		stall_req<=`StallNo
+
 	end else begin
 		case (op)
 			`OP_ADDSP3:begin
-			
+							
 			end
 			`OP_NOP:begin
 				//default nop	
@@ -183,7 +217,39 @@ always @ (*) begin
 			end
 		endcase
 	end
+end
 
+
+always @ (*) begin
+	if (rst ==`RstEnable) begin
+		reg1_o<=`ZeroWord;
+	end else if (reg1_re_o==`ReadEnable && ex_we_i == `WriteEnable && reg1_addr_o == ex_waddr_i) begin
+		reg1_o<=ex_wdata_i;
+	end else if (reg1_re_o==`ReadEnable && mem_we_i == `WriteEnable && reg1_addr_o == mem_waddr_i) begin
+		reg1_o<=mem_wdata_i;
+	end else if (reg1_re_o==`ReadEnable) begin
+		reg1_o<=reg1_data_i;
+	end else if (reg1_re_o==`ReadDisable) begin
+		reg1_o<=imm_num;
+	end else begin
+		reg1_o<=`ZeroWord;
+	end
+end
+
+always @ (*) begin
+	if (rst ==`RstEnable) begin
+		reg2_o<=`ZeroWord;
+	end else if (reg2_re_o==`ReadEnable && ex_we_i == `WriteEnable && reg2_addr_o == ex_waddr_i) begin
+		reg2_o<=ex_wdata_i;
+	end else if (reg2_re_o==`ReadEnable && mem_we_i == `WriteEnable && reg2_addr_o == mem_waddr_i) begin
+		reg2_o<=mem_wdata_i;
+	end else if (reg2_re_o==`ReadEnable) begin
+		reg2_o<=reg2_data_i;
+	end else if (reg2_re_o==`ReadDisable) begin
+		reg2_o<=imm_num;
+	end else begin
+		reg2_o<=`ZeroWord;
+	end
 end
 
 endmodule
