2196d025b3a52b9071fbb3c9bb397558ff49366b
ahaoyunqiu <shaoyunqiu@163.com>
2016-11-19 10:04:02 +0800

    change exe_mem, add memrw and memaddr

--- src/ex_mem.v
+++ src/ex_mem.v
@@ -4,12 +4,16 @@ module ex_mem(
 	input wire clk,
 	input wire rst,
 	input wire stall,
+	input wire[`MemRWBus] ex_memrw,
+	input wire[`DataAddrBus] ex_memaddr,
 	input wire[`DataBus] ex_wdata,
 	input wire[`RegAddrBus] ex_waddr,
 	input wire ex_we,
 	
 	output reg[`DataBus] mem_wdata,
 	output reg[`RegAddrBus] mem_waddr,
+	output reg[`MemRWBus] mem_memrw,
+	output reg[`DataAddrBus] mem_memaddr,
 	output reg mem_we
 );
 
@@ -17,14 +21,18 @@ always @ (posedge clk) begin
 	if (rst == `RstEnable) 
 	begin
 		mem_wdata<=`ZeroData;
-		mem_waddr<=`ZeroDataAddr;
+		mem_waddr<= `ZeroRegAddr ;
 		mem_we<=`WriteDisable;
+		mem_memrw <= `MemRW_Idle ;
+		mem_memaddr <= `ZeroDataAddr ;
 	end
 	else if (stall == `StallNo)
 	begin
 		mem_waddr<=ex_waddr;
 		mem_wdata<=ex_wdata;
 		mem_we<=ex_we;
+		mem_memrw <= ex_memrw ;
+		mem_memaddr <= ex_memaddr ;
 	end
 end
 
