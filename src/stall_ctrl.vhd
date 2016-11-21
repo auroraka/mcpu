4a1669d32e68075ba5b4d1e84b05a130208e1434
ahaoyunqiu <shaoyunqiu@163.com>
2016-11-21 09:28:38 +0800

    change stall_ctrl

--- src/stall_ctrl.vhd
+++ src/stall_ctrl.vhd
@@ -6,17 +6,39 @@ entity stall_ctrl is
 	Port(
 		rst : in STD_LOGIC ;
 		stallreq_id: in STD_LOGIC ;
-		stallreq__ex: in STD_LOGIC ;
 		stallreq_mem: in STD_LOGIC ;
 		
 		stall_pc: out STD_LOGIC ;
-		stall_if_id: out STD_LOGIC ;
-		stall_id_ex: out STD_LOGIC ;
-		stall_ex_mem: out STD_LOGIC ;
-		stall_mem_wb: out STD_LOGIC 
+		stall_id: out STD_LOGIC ;
+		stall_ex: out STD_LOGIC ;
+		stall_mem: out STD_LOGIC 
 	) ;
 end stall_ctrl ;
 
 architecture Behavioral of stall_ctrl is
+
 begin
+	process(rst, stallreq_id, stallreq_mem)
+	variable tmp_pc : STD_LOGIC := StallNo ;
+	variable tmp_id : STD_LOGIC := StallNo ;
+	variable tmp_ex : STD_LOGIC := StallNo ;
+	variable tmp_mem : STD_LOGIC := StallNo ;
+	begin
+		tmp_pc := StallNo ;
+		tmp_id := StallNo ;
+		tmp_ex := StallNo ;
+		tmp_mem := StallNo ;
+		if(rst = RstDisable) then
+			if(stallreq_mem = StallYes) then
+				tmp_pc := StallYes ;
+			elsif (stallreq_id = StallYes) then
+				tmp_id := StallYes ;
+				tmp_pc := StallYes ;
+			end if ;
+		end if ;
+		stall_pc <= tmp_pc ;
+		stall_id <= tmp_id ;
+		stall_ex <= tmp_ex ;
+		stall_mem <= tmp_mem ;
+	end process ;
 end Behavioral ;
