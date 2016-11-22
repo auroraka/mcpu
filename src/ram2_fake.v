15e47332e32d6d8063f41cf46454e28dd67e4c65
Auroraka <yangtianlong111@gmail.com>
2016-11-22 17:12:31 +0800

    modify ram2 fake

--- src/ram2_fake.v
+++ src/ram2_fake.v
@@ -2,6 +2,8 @@
 
 module ram2_fake(
 	input wire clk,
+	input wier[`DataAddrBus] in,
+	output wire[`InstBus] inst,
 	input wire[`DataBus] mem_data_i,
 	output reg[`DataBus] mem_data_o,
 	input wire[`DataAddrBus] mem_addr_i,
@@ -15,13 +17,11 @@ module ram2_fake(
 	initial $readmemh ( "ram2.data", ram );
 
 	always @ (*) begin
-		if (mem_ce == `RamChipDisable) begin
+		if (mem_ce == `RamChipEnable) begin //mem rw
 			mem_data_o <= `ZeroWord;
-	  	end else begin
+	  	end else begin// pc rw
 	  		if (mem_re == `ReadEnable) begin
 	  			mem_data_o <= ram[mem_addr_i[11:0]];
-			end else if (mem_we == `WriteEnable) begin
-				ram[mem_addr_i[11:0]]<=mem_data_i;
 			end else begin
 				mem_data_o <= `ZeroWord;
 			end
