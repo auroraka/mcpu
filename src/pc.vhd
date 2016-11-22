64c9a198827cc2e6a7ff5765d627bd06553f5fd7
Auroraka <yangtianlong111@gmail.com>
2016-11-22 15:01:56 +0800

    modify some mcpu

--- src/pc.vhd
+++ src/pc.vhd
@@ -12,7 +12,7 @@ entity pc is port(
 	rst:		in 		std_logic;
 	branch_flag_o:	in	std_logic;
 	branch_addr_o:	in	InstAddrBus;
-	pc_o:				out InstAddrBus
+	pc:				out InstAddrBus
 );
 end pc;
 
@@ -24,15 +24,15 @@ begin
 	begin
 		if(rst = RstEnable) then
 			pc_v := ZeroInstAddr ;
-			pc_o <= pc_v ;
+			pc <= pc_v ;
 		elsif (clk'event and clk = '1') then
 			--pc <= pc_v ; -- update pc
 			if (stall = StallNo) then 
 				if branch_flag_o = BranchFlagUp then -- jump into new pc ;
-					pc_o <= branch_addr_o ;
+					pc <= branch_addr_o ;
 					pc_v := branch_addr_o;
 				else 
-					pc_o <= pc_v ;
+					pc <= pc_v ;
 				end if;
 				-- prepare pc_v for next pc 
 				pc_v := pc_v + 1;
